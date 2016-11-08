4644310d2c37a45d71d78de49a2dc62ba306cb50
managedcoder <ruwill@live.com>
2016-11-07 22:19:44 -0500

    Added ARM Template

--- VSSolutionTemplates/templates/JumpStreetMobile/JumpStreetMobile.ARM/Scripts/Deploy-AzureResourceGroup.ps1
+++ VSSolutionTemplates/templates/JumpStreetMobile/JumpStreetMobile.ARM/Scripts/Deploy-AzureResourceGroup.ps1
@@ -4,7 +4,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'JumpStreetARMTemplate',
+    [string] $ResourceGroupName = 'JumpStreetMobile.ARM',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',

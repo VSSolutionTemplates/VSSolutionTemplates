c6cea81a577195e98c54053a06b1294362c9325a
ruwill <ruwill@LUMPY>
2016-11-29 08:55:23 -0500

    Updated pw template with new project GUIDs since the .vsix would not publish on AppVeyor and I assume that is because the project GUID did not match the GUIDs in the solution file

--- VSSolutionTemplates/templates/JumpStreetMobile/pw-templateinfo.ps1
+++ VSSolutionTemplates/templates/JumpStreetMobile/pw-templateinfo.ps1
@@ -15,13 +15,13 @@ $templateInfo = New-Object -TypeName psobject -Property @{
 $templateInfo | replace (
     ('JumpStreetMobile', {"$ProjectName"}, {"$DefaultProjectName"} ,@('*.sln','*.ps1','*.vstemplate','*.csproj','*.bak','*.cs','*.xml','*.plist','*.projitems','*.shproj','*.xaml','*.config','*.appxmanifest','*.deployproj','json') ),
 
-    ('8EBB17C5-5B87-466B-99BE-709C04F71BC8', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
-    ('B095DC2E-19D7-4852-9450-6774808B626E', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
-    ('e651c0cb-f5fb-4257-9289-ef45f3c1a02c', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
-    ('1dfffd59-6b32-4937-bfde-1e10c11d22c3', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
-    ('4D2348EA-44AA-479F-80FB-EF67D64F4F3A', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
-    ('0A7800A3-784F-4822-8956-7BAC2C4D194E', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
+    ('3AFA591D-C115-4AC9-9519-F788373ECDE9', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
+    ('5FA0219F-B063-4D07-800B-CC38BDB5B916', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
+    ('34B9099E-4B00-4707-BFDE-0B95997F8893', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
+    ('0483302A-9154-434A-93EB-5633AAFD4409', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
+    ('059786BD-D4DA-4770-8624-13A84BFC97AC', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
     ('6B0A711C-8401-4240-BA08-A8198EFC271E', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
+    ('209FA716-A7AD-4095-BD70-C8710FC66FA7', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json')),
     ('68D82A27-F8EA-4550-9AC7-766CC4CA164E', {$null}, {[System.Guid]::NewGuid()},@('*.sln','*.ps1','*.csproj','*.bak','*.projitems','*.shproj','*.deployproj','json'))
 )
 
@@ -47,13 +47,13 @@ Use this one-liner to figure out the include expression for the project name
 Use this one-liner to figure out the guids in your template
 > Get-ChildItem .\VSSolutionTemplates\templates *.*proj -Recurse -File|Select-Object -ExpandProperty fullname -Unique|% { ([xml](Get-Content $_)).Project.PropertyGroup.ProjectGuid|Select-Object -Unique|%{ '({0}, {{"$ProjectId"}}, {{[System.Guid]::NewGuid()}},@("*.*proj")),' -f $_ }}
 
-({8EBB17C5-5B87-466B-99BE-709C04F71BC8}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
-({B095DC2E-19D7-4852-9450-6774808B626E}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
-(e651c0cb-f5fb-4257-9289-ef45f3c1a02c, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
-(1dfffd59-6b32-4937-bfde-1e10c11d22c3, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
-({4D2348EA-44AA-479F-80FB-EF67D64F4F3A}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
-({0A7800A3-784F-4822-8956-7BAC2C4D194E}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
+({3AFA591D-C115-4AC9-9519-F788373ECDE9}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
+({5FA0219F-B063-4D07-800B-CC38BDB5B916}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
+({34B9099E-4B00-4707-BFDE-0B95997F8893}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
+({0483302A-9154-434A-93EB-5633AAFD4409}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
+({059786BD-D4DA-4770-8624-13A84BFC97AC}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
 ({6B0A711C-8401-4240-BA08-A8198EFC271E}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
+({209FA716-A7AD-4095-BD70-C8710FC66FA7}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
 ({68D82A27-F8EA-4550-9AC7-766CC4CA164E}, {"$ProjectId"}, {[System.Guid]::NewGuid()},@("*.*proj")),
 
 
